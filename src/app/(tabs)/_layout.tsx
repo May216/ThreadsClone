cd5bc18f4e1c24b43d032893715b16215df37e1f
Jenna <jenna@brilltek.com.tw>
2025-05-22 14:23:29 +0800

    feat: navigation dark theme

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,45 @@
+import { Tabs } from "expo-router"
+import { Feather } from "@expo/vector-icons"
+
+export default function TabsLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarShowLabel: false }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="search" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="notifications"
+        options={{
+          title: "Notifications",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="heart" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="user" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
+  )
+}
